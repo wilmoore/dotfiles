aa3e582e8205f8f7c50b0c71553a579a716d1f1b
Wil Moore III <wil.moore@wilmoore.com>
2014-06-08 23:04:05 -0600

    modify bootstrap.sh to include running applescripts

--- active/.runscript/bootstrap.sh
+++ active/.runscript/bootstrap.sh
@@ -1,7 +1,18 @@
+# shell scripts
 if [ -d ./bootstrap ]; then
   for i in ./bootstrap/*.sh; do
     if [ -r $i ]; then
       . $i
+    fi
+  done
+  unset i
+fi
+
+# apple scripts
+if [ -d ./bootstrap ]; then
+  for i in ./bootstrap/*.scpt; do
+    if [ -r $i ]; then
+      osascript $i
     fi
   done
   unset i
